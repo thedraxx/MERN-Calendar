d0fb88292025f6bcf9c29073f99ba03d89c67dda
Frank <55867157+thedraxx@users.noreply.github.com>
2022-07-15 15:23:54 -0300

    add loginScreen style and navbar

---
+++ src/calendar/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const Navbar = () => {
+  return (
+    <div className="navbar navbar-dark bg-dark mb-4 px-4">
+      <span className="navbar-brand">
+        <i className="fas fa-calendar-alt"></i>
+        &nbsp; Fernando
+      </span>
+      <button className="btn btn-outline-danger">
+        <i className="fas fa-sign-out-alt">
+          <span>Salir</span>
+        </i>
+      </button>
+    </div>
+  );
+};
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -4,7 +4,7 @@ import { LoginPage } from "../auth";
 import { CalendarPage } from "../calendar";
 
 export const AppRouter = () => {
-  const authStatus = "not-authenticated";
+  const authStatus = "authenticated";
 
   return (
     <div>
@@ -14,7 +14,7 @@ export const AppRouter = () => {
           <Route path="/auth/*" element={<LoginPage />} />
         ) : (
           // Si estas autenticado te manda al CalendarPage
-          <Route path="/calendar" element={<CalendarPage />} />
+          <Route path="/*" element={<CalendarPage />} />
         )}
         {/* Si no estas autenticado y poner cualquier cosa te redirige a auth/login */}
         <Route path="/*" element={<Navigate to="./auth/login" />} />
